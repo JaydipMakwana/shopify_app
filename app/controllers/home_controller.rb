c6ddc1cd6595ce08efff5be9499df662806fe52f
JaydipMakwana <jaydip.makwana@botreetechnologies.com>
2020-02-19 14:43:32 +0530

    Theme setup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@
 
 class HomeController < AuthenticatedController
   def index
+  end
+
+  def product_listing
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
+  get 'product_listing', to: 'home#product_listing'
   mount ShopifyApp::Engine, at: '/'
   # post 'customers/import', to: 'home#import_customers', as: :import_customers
   resources :customers, only: %i[index] do
